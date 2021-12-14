74d513b9aa4fc6209bf2f1992d9ea8af06a01cfe
Antony <vontonnie@gmail.com>
2021-12-14 06:47:52 +0300

    Add Kotlin Extensions Plugin

--- app/src/main/java/com/uxstate/recipeapp/MainActivity.kt
+++ app/src/main/java/com/uxstate/recipeapp/MainActivity.kt
@@ -3,12 +3,29 @@ package com.uxstate.recipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.uxstate.recipeapp.ui.theme.RecipeAppTheme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +43,55 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
+    Column(modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF2F2F2))) {
+
+
+
+
+            Image(
+                painter = painterResource(id = R.drawable.happy_meal_small),
+                contentDescription = null,
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+            )
+
+
+
+       Row(
+           Modifier
+                   .fillMaxWidth()
+                   .padding(10.dp),
+           horizontalArrangement = Arrangement.SpaceBetween
+       ) {
+           Text(
+               text = "Happy Meal",
+               textAlign = TextAlign.Start,
+               fontSize = 20.sp,
+               fontWeight = FontWeight.W600,
+
+           )
+           Text(
+               text = "$5.99",
+               fontSize = 16.sp,
+               modifier = Modifier.align(CenterVertically),
+               color = Color.Green
+           )
+       }
+        
+        Text(text = "800 Calories", modifier = Modifier.padding(10.dp))
+        Button(onClick = { /*TODO*/ }, modifier = Modifier
+                .padding(10.dp)
+                .align(CenterHorizontally)) {
+            Text(text = "Order Now")
+        }}
+    }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
@@ -36,3 +100,27 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+
+/* Column(modifier = Modifier.verticalScroll(state = scrollState).background(color = Color(0xFFF2F2F2)).fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.happy_meal_small),
+            contentDescription = null,
+            modifier = Modifier.height(300.dp),
+            contentScale = ContentScale.Crop
+        )
+
+
+        Column(modifier = Modifier.padding(16.dp)) {
+
+
+            Text(text = "Happy Meal", style = TextStyle(fontSize = 26.sp))
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "800 Calories",style = TextStyle(fontSize = 17.sp))
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "$5.99",style = TextStyle(fontSize = 17.sp))
+        }
+
+
+
+    }*/
