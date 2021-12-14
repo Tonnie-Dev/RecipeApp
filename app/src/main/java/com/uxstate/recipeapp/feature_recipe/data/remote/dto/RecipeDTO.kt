5d6f1ad6059e74694eb9cdd7e10c7877b1f982d9
Antony <vontonnie@gmail.com>
2021-12-14 08:11:25 +0300

    Refactor Result data class to RecipeDTO data class

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/data/remote/dto/RecipeDTO.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/data/remote/dto/RecipeDTO.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class Result(
+data class RecipeDTO(
     @Json(name = "cooking_instructions")
     val cookingInstructions: Any?,
     @Json(name = "date_added")
