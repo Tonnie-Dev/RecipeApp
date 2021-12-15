e21c6b0be10943051eb779a898a32e50c51a1ee9
Antony <vontonnie@gmail.com>
2021-12-15 14:20:29 +0300

    Add RecipeRepositoryImpl class

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/repository/RecipeRepository.kt
@@ -1,9 +1,10 @@
 package com.uxstate.recipeapp.feature_recipe.domain.repository
 
 import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
+import com.uxstate.recipeapp.feature_recipe.domain.model.RecipesResult
 
 interface RecipeRepository {
 
-    suspend fun getRecipes()
+    suspend fun getRecipes(): RecipesResult
     suspend fun getRecipeById():Recipe
 }
