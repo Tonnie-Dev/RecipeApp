094f2dd5645a110bfb5cd78272ef02ace380cfee
Antony <vontonnie@gmail.com>
2021-12-15 14:06:09 +0300

    Add Repo defin methods

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/repository/RecipeRepository.kt
@@ -1,4 +1,9 @@
 package com.uxstate.recipeapp.feature_recipe.domain.repository
 
+import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
+
 interface RecipeRepository {
+
+    suspend fun getRecipes()
+    suspend fun getRecipeById():Recipe
 }
