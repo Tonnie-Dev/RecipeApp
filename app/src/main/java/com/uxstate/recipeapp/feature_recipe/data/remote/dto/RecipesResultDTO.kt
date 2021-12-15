5e85c6eadebdcfac8c0f99e668f9971f8d06987c
Antony <vontonnie@gmail.com>
2021-12-15 14:14:08 +0300

    Add toRecipesResult mapper ext fxn

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/data/remote/dto/RecipesResultDTO.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/data/remote/dto/RecipesResultDTO.kt
@@ -3,9 +3,15 @@ package com.uxstate.recipeapp.feature_recipe.data.remote.dto
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import com.uxstate.recipeapp.feature_recipe.domain.model.RecipesResult
 
 @JsonClass(generateAdapter = true)
 data class RecipesResultDTO(
     val count: Int,
     val results: List<RecipeDTO>
 )
+
+fun RecipesResultDTO.toRecipesResult():RecipesResult{
+
+    return RecipesResult(count = count, results = results)
+}
--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/model/RecipesResult.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/model/RecipesResult.kt
@@ -1,3 +1,6 @@
 package com.uxstate.recipeapp.feature_recipe.domain.model
 
-data class RecipesResult()
+import com.uxstate.recipeapp.feature_recipe.data.remote.dto.RecipeDTO
+
+data class RecipesResult(val count: Int,
+                         val results: List<RecipeDTO>)
