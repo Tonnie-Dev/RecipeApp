e1dd49b7fa6e0f1ad52e83b58474c47f136cfca8
Antony <vontonnie@gmail.com>
2021-12-15 09:28:41 +0300

    Add searchRecipes() fxn to RecipeAPI

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/data/remote/RecipeAPI.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/data/remote/RecipeAPI.kt
@@ -1,4 +1,34 @@
 package com.uxstate.recipeapp.feature_recipe.data.remote
 
+import com.uxstate.recipeapp.feature_recipe.data.remote.dto.RecipeDTO
+import com.uxstate.recipeapp.feature_recipe.data.remote.dto.RecipeSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
 interface RecipeAPI {
+
+
+    companion object {
+
+        const val BASE_URL = "https://food2fork.ca/api/recipe/"
+
+    }
+
+//search all recipes
+    @GET(value = "/search")
+    suspend fun searchRecipes(
+        @Header("Authorization")
+        token: String,
+        @Query("page")
+        page: Int,
+        @Query("query")
+        query:String
+    ): RecipeSearchResponse
+
+
+    //search a single recipe
+
+
+   
 }
