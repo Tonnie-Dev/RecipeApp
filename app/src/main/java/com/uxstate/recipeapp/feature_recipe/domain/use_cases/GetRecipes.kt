8deb562e5da73a04db920e9901496fb2138cabda
Antony <vontonnie@gmail.com>
2021-12-15 16:54:07 +0300

    Add GetRecipes use case

---
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/use_cases/GetRecipes.kt
@@ -0,0 +1,20 @@
+package com.uxstate.recipeapp.feature_recipe.domain.use_cases
+
+import com.uxstate.recipeapp.core.util.Resource
+import com.uxstate.recipeapp.feature_recipe.domain.model.RecipesResult
+import com.uxstate.recipeapp.feature_recipe.domain.repository.RecipeRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
+class GetRecipes (private val repository: RecipeRepository){
+
+    operator fun invoke(  token: String,
+                          page: Int,
+                          query: String): Flow<Resource<RecipesResult>> = flow{
+
+
+                              repository.getRecipes(token, page, query)
+    }
+
+}
