4600cd2794cd645014d738ce69504063ffe22ba8
Antony <vontonnie@gmail.com>
2021-12-15 15:47:07 +0300

    Refactor RecipeRepository parameters

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/data/remote/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/data/remote/repository/RecipeRepositoryImpl.kt
@@ -1,19 +1,23 @@
 package com.uxstate.recipeapp.feature_recipe.data.remote.repository
 
+import com.uxstate.recipeapp.core.util.Resource
 import com.uxstate.recipeapp.feature_recipe.data.remote.RecipeAPI
 import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
 import com.uxstate.recipeapp.feature_recipe.domain.model.RecipesResult
 import com.uxstate.recipeapp.feature_recipe.domain.repository.RecipeRepository
+import kotlinx.coroutines.flow.Flow
 
 class RecipeRepositoryImpl(val api: RecipeAPI) :RecipeRepository{
-    override suspend fun getRecipes(): RecipesResult {
-
-        api.getRecipes()
-
-
+    override suspend fun getRecipes(
+        token: String,
+        page: Int,
+        query: String
+    ): Flow<Resource<RecipesResult>> {
+        TODO("Not yet implemented")
     }
 
-    override suspend fun getRecipeById(id:Int): Recipe {
-       return api.getRecipeById(id)
+    override suspend fun getRecipeById(token: String, id: Int): Flow<Resource<Recipe>> {
+        TODO("Not yet implemented")
     }
+
 }
--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/repository/RecipeRepository.kt
@@ -1,11 +1,12 @@
 package com.uxstate.recipeapp.feature_recipe.domain.repository
 
+import com.uxstate.recipeapp.core.util.Resource
 import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
 import com.uxstate.recipeapp.feature_recipe.domain.model.RecipesResult
 import kotlinx.coroutines.flow.Flow
 
 interface RecipeRepository {
 
-    suspend fun getRecipes(): Flow<RecipesResult>
-    suspend fun getRecipeById():Flow<Recipe>
+    suspend fun getRecipes(token:String, page: Int, query:String): Flow<Resource<RecipesResult>>
+    suspend fun getRecipeById(token: String, id:Int):Flow<Resource<Recipe>>
 }
