8098456556afe528ff30a39358c1bafbf4d5f359
Antony <vontonnie@gmail.com>
2021-12-15 17:00:15 +0300

    Add GetRecipeByIdUseCase

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/use_cases/GetRecipeByIdUseCase.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/domain/use_cases/GetRecipeByIdUseCase.kt
@@ -1,4 +1,18 @@
 package com.uxstate.recipeapp.feature_recipe.domain.use_cases
 
-class GetRecipeByIdUseCase {
+import com.uxstate.recipeapp.core.util.Resource
+import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
+import com.uxstate.recipeapp.feature_recipe.domain.repository.RecipeRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetRecipeByIdUseCase(private val repository: RecipeRepository) {
+
+    operator fun invoke(token: String, id: Int): Flow<Resource<Recipe>> = flow {
+
+
+        repository.getRecipeById(token = token, id = id)
+    }
+
+
 }
