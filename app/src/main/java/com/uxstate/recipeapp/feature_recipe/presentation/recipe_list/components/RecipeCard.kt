984f9475d22411f12cb5dadadb220740fdbdd20f
Antony <vontonnie@gmail.com>
2021-12-16 18:07:54 +0300

    Add Rows to Card

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/RecipeCard.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/RecipeCard.kt
@@ -1,13 +1,19 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.uxstate.recipeapp.R
 import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
 
 @Composable
@@ -23,6 +29,53 @@ fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
     ) {
 
 
+        Column (modifier = Modifier.fillMaxHeight()){
+
+            Image(
+                painter = painterResource(id = R.drawable.empty_plate),
+                contentScale = ContentScale.Crop,
+                contentDescription = null,
+                modifier = Modifier.height(225.dp),
+                alignment = Alignment.TopCenter
+            )
+
+
+            Row (horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 12.dp)){
+
+                Text(text = recipe.title)
+                Text(text = recipe.rating.toString())
+            }
+
+
+        }
+
+
+
+
     }
+
+
+}
+
+@Preview(name = "Card")
+@Composable
+fun PreviewCard() {
+
+    val recipe = Recipe(
+        id = 0,
+        title = "Salad",
+        publisher = "",
+        featuredImage = "www",
+        rating = 13,
+        sourceUrl = "",
+        description = "Cooking Salad",
+        cookingInstructions = "",
+        ingredients = listOf(),
+        dateAdded = "",
+        dateUpdate = ""
+    )
+
+
+    RecipeCard(recipe, {})
 
 }
