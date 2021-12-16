e0f86488ec99957e5122238b7650362bf7d1a275
Antony <vontonnie@gmail.com>
2021-12-16 19:03:52 +0300

    Show error text

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/RecipesListScreen.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/RecipesListScreen.kt
@@ -1,15 +1,17 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components.RecipeCard
@@ -45,7 +47,15 @@ fun RecipesListScreen(
         }
 
         if (listState.error.isNotEmpty()){
-
+            Text(
+                text = listState.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                        .align(Alignment.Center))
+        }
 
 
         }
