d99f92ec7897173f11d9f4f4e3936c40b43f879d
Antony <vontonnie@gmail.com>
2021-12-16 18:55:40 +0300

    Add Lazy Column

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/RecipesListScreen.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/RecipesListScreen.kt
@@ -1,6 +1,11 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
@@ -10,4 +15,18 @@ fun RecipesListScreen(
     viewModel: RecipesListViewModel = hiltViewModel()
 ) {
 
+
+    //get state from viewModel
+    val listState by viewModel.recipesListState
+
+
+    //add containing column
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            
+        }
+
+    }
 }
