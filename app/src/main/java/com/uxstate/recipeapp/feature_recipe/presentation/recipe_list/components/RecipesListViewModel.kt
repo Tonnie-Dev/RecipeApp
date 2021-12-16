806dedec1c8e5922b812280377520bddd34769c8
Antony <vontonnie@gmail.com>
2021-12-16 10:34:30 +0300

    Add RecipesListViewModel Class

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/RecipesListViewModel.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/RecipesListViewModel.kt
@@ -1,4 +1,11 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
-class RecipesListViewModel {
+import androidx.lifecycle.ViewModel
+import com.uxstate.recipeapp.feature_recipe.domain.use_cases.GetRecipesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipesListViewModel @Inject constructor(useCase:GetRecipesUseCase) :
+    ViewModel() {
 }
