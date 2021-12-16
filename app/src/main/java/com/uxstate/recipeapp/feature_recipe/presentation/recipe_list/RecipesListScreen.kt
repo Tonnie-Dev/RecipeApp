e3cbbf1f5db01c25a650a89779c217fa9bf21e57
Antony <vontonnie@gmail.com>
2021-12-16 18:58:37 +0300

    Call RecipeCard inside the LazyColumn

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/RecipesListScreen.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/RecipesListScreen.kt
@@ -3,11 +3,13 @@ package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components.RecipeCard
 
 @Composable
 fun RecipesListScreen(
@@ -25,7 +27,13 @@ fun RecipesListScreen(
     Column(modifier = Modifier.fillMaxSize()) {
 
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            
+
+            items(items = listState.recipes){
+                recipe ->
+
+                RecipeCard(recipe = recipe, onClick = {})
+            }
+
         }
 
     }
