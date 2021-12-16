9ffb015c107c28113fc54b01cb99448a5e859463
Antony <vontonnie@gmail.com>
2021-12-16 10:33:06 +0300

    Add RecipeListState data class

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/RecipeListState.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/RecipeListState.kt
@@ -1,4 +1,10 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
-data class RecipeListState(val loading:Boolean = false, val error:String = "", val recipesList:) {
+import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
+
+data class RecipeListState(
+    val loading: Boolean = false,
+    val error: String = "",
+    val recipes: List<Recipe> = emptyList()
+) {
 }
