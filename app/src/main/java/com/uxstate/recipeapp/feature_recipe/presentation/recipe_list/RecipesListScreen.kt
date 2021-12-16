91614dec699d6186dc0a5d080cfcef95ebfc7c00
Antony <vontonnie@gmail.com>
2021-12-16 18:38:25 +0300

    Add RecipesListScreen parameters

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/RecipesListScreen.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/RecipesListScreen.kt
@@ -1,8 +1,13 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 
 @Composable
-fun RecipesListScreen() {
-    
+fun RecipesListScreen(
+    navController: NavController,
+    viewModel: RecipesListViewModel = hiltViewModel()
+) {
+
 }
