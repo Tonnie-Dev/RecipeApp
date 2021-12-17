3d98ab995afb7fcb9bca9e474dcb73ebe975d5e0
Antony <vontonnie@gmail.com>
2021-12-17 21:15:26 +0300

    Add Surface for the SearchTextField

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/SearchTextField.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/SearchTextField.kt
@@ -1,14 +1,28 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun SearchTextField(value: String, onValueChanged: (String) -> Unit, modifier: Modifier=Modifier) {
-
-
-    TextField(value = value, onValueChange = onValueChanged, modifier.fillMaxWidth())
+fun SearchTextField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        elevation = 8.dp,
+        modifier = Modifier.fillMaxWidth(),
+        color = MaterialTheme.colors.onSurface
+    ) {
+        TextField(value = value, onValueChange = onValueChanged, modifier.fillMaxWidth())
+    }
 
 }
+
+   
+
