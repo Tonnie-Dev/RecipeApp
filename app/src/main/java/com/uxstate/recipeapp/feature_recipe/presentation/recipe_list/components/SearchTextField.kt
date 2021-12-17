35c53bcfdca07a69b93c791181b739b938b6e1eb
Antony <vontonnie@gmail.com>
2021-12-17 10:53:26 +0300

    Add SearchTextField Composable

---
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/SearchTextField.kt
@@ -0,0 +1,14 @@
+package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchTextField(value: String, onValueChanged: (String) -> Unit) {
+
+
+    TextField(value = value, onValueChange = onValueChanged, modifier = Modifier.fillMaxWidth())
+
+}
