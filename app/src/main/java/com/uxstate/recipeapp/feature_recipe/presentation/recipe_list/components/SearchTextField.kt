f4124d7bb0e4d02a255d80949ce8f76055573645
Antony <vontonnie@gmail.com>
2021-12-17 21:51:06 +0300

    Add keyboardActions

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/SearchTextField.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/SearchTextField.kt
@@ -1,45 +1,69 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
+@ExperimentalComposeUiApi
 @Composable
 fun SearchTextField(
+    modifier: Modifier = Modifier,
     value: String,
     onValueChanged: (String) -> Unit,
-    modifier: Modifier = Modifier
-) {
-  Surface(
+    onImeAction: () -> Unit = {},
+
+    ) {
+
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
         elevation = 8.dp,
         modifier = Modifier.fillMaxWidth(),
         color = MaterialTheme.colors.surface
     ) {
 
 
-      TextField(
-          value = value,
-          onValueChange = onValueChanged,
+        TextField(
+            value = value,
+            onValueChange = onValueChanged,
 
-          //occupy 95% of max width to menu space
-          modifier.fillMaxWidth(0.95f),
+            //occupy 95% of max width to menu space
+            modifier.fillMaxWidth(0.95f),
 
-          label = {Text(text = "search")},
+            label = { Text(text = "search") },
 
-          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text, imeAction = ImeAction.Done)
-      )
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
 
-  }
+
+            leadingIcon = {
+
+                Icon(imageVector = Icons.Filled.Search, contentDescription = null)
+            },
+
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onImeAction()
+                    keyboardController?.hide()
+
+                })
+
+        )
+
     }
+}
 
 
 
