b841d409b83c2093587ae24e9812738784dc3a79
Antony <vontonnie@gmail.com>
2021-12-19 19:33:05 +0300

    Add Brush Variable

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/ShimmerRecipeCardItem.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/ShimmerRecipeCardItem.kt
@@ -1,2 +1,25 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun ShimmerRecipeCardItem(
+    color:List<Color>,
+    cardHeight:Dp
+) {
+
+    val brush = Brush.linearGradient(
+        colors = color,
+        start = Offset(200f, 200f),
+        end = Offset(200f, 200f)
+    )
+
+Surface() {
+
+    }
+}
