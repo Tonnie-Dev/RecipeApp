60bbd7e1060b3dfb0afe45597fba7690a7d15cbf
Antony <vontonnie@gmail.com>
2021-12-19 20:39:00 +0300

    Delete initial code for ShimmerRecipeCardItem

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/ShimmerRecipeCardItem.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/ShimmerRecipeCardItem.kt
@@ -1,40 +1,14 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 
 @Composable
 fun ShimmerRecipeCardItem(
-    colors:List<Color>,
-    cardHeight:Dp
+
 ) {
 
-    val brush = Brush.linearGradient(
-        colors = colors,
-        start = Offset(200f, 200f),
-        end = Offset(400f, 400f)
-    )
 
-Surface(shape = MaterialTheme.shapes.small) {
-    //surface with one child - Spacer
-
-    //spacer matches the dimens of recipe card
-    Spacer(
-        modifier = Modifier
-                .fillMaxWidth()
-                .height(cardHeight)
-                .background(brush = brush)
-    )
 
     }
-}
+
+
