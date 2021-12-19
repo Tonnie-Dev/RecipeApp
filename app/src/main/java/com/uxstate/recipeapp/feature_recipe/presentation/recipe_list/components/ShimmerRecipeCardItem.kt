e173d12e82e4bdfa73a6f98794704df7b5b150d8
Antony <vontonnie@gmail.com>
2021-12-19 20:44:31 +0300

    Create Spacer Objects for animations

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/ShimmerRecipeCardItem.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/ShimmerRecipeCardItem.kt
@@ -1,14 +1,36 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ShimmerRecipeCardItem(
-
+    brush: Brush
 ) {
 
+    Column(modifier = Modifier.padding(16.dp)) {
 
+        Spacer(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+                    .background(brush = brush)
+        )
+        Spacer(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp)
+                    .padding(vertical = 8.dp)
+                    .background(brush = brush)
+        )
 
     }
+
+
+}
 
 
