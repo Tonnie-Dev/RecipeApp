2983ea821e179915130c562f8378e3a3e37b27fa
Antony <vontonnie@gmail.com>
2021-12-19 17:01:14 +0300

    Copy-Paste Pulsating Composable

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/PulsingDemo.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/PulsingDemo.kt
@@ -1,11 +1,28 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 
-//object cannot be instantiated
-object PulsingAnimationDefinitions {
 
-    //define animations states in an enum
+@Composable
+    fun Pulsating(pulseFraction: Float = 1.2f, content: @Composable () -> Unit) {
+        val infiniteTransition = rememberInfiniteTransition()
 
-    enum class PulseSate { INITIAL, FINAL}
+        val scale by infiniteTransition.animateFloat(
+            initialValue = 1f,
+            targetValue = pulseFraction,
+            animationSpec = infiniteRepeatable(
+                animation = tween(1000),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
 
-}
+        Box(modifier = Modifier.scale(scale)) {
+            content()
+        }
+    }
+
