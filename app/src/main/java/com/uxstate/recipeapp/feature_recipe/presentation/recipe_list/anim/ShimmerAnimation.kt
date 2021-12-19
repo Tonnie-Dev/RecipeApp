4a8f5b633f85393c6ebe2db298f75aed2e824c67
Antony <vontonnie@gmail.com>
2021-12-19 20:57:17 +0300

    Define translate Animation

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/anim/ShimmerAnimation.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/anim/ShimmerAnimation.kt
@@ -1,8 +1,28 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.anim
 
+import androidx.compose.animation.core.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 
 @Composable
 fun ShimmerAnimation() {
-    
+
+    /*
+        Create InfiniteTransition
+        which holds child animation like [Transition]
+        animations start running as soon as they enter
+        the composition and do not stop unless they are removed
+       */
+
+
+    val transition = rememberInfiniteTransition()
+    val translateAnim by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1200, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
 }
