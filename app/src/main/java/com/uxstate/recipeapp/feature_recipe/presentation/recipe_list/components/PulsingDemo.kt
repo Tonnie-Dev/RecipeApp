189637c8b189b9c03a47c972c580c63edb10f3fc
Antony <vontonnie@gmail.com>
2021-12-19 16:35:04 +0300

    Define animations states in an enum

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/PulsingDemo.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/PulsingDemo.kt
@@ -1,2 +1,11 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+
+//object cannot be instantiated
+object PulsingAnimationDefinitions {
+
+    //define animations states in an enum
+
+    enum class PulseSate { INITIAL, FINAL}
+
+}
