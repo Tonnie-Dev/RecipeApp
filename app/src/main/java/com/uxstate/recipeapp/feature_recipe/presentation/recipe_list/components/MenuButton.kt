7c7b7f4e46dd9ee1bf42f1bda515406d505a6234
Antony <vontonnie@gmail.com>
2021-12-20 19:02:03 +0300

    Add constraint layout compose dependency

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MenuButton.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MenuButton.kt
@@ -1,2 +1,15 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+@Composable
+fun MenuButton(
+    modifier: Modifier = Modifier,
+    onToggleTheme: (Boolean) -> Unit
+) {
+
+    ConstraintLayout()
+}
