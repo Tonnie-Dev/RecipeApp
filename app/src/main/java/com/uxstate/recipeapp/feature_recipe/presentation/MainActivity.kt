d2527ada3c0d59048140cdd6eac789a7ad31da72
Antony <vontonnie@gmail.com>
2021-12-20 04:39:31 +0300

    Change Status bar color to blue 800

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/MainActivity.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.RecipesOverviewScreen
+import com.uxstate.recipeapp.feature_recipe.presentation.ui.theme.Blue800
 import com.uxstate.recipeapp.feature_recipe.presentation.ui.theme.RecipeAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,7 +23,7 @@ class MainActivity() : ComponentActivity() {
 
         setContent {
             RecipeAppTheme {
-
+                window?.statusBarColor = Blue800.toArgb()
 
                 val navController = rememberNavController()
 
