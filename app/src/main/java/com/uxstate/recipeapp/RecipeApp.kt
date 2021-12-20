2a8a55e69a4ac9855acfe6710fdbe7681ad26423
Antony <vontonnie@gmail.com>
2021-12-20 19:15:42 +0300

    Create toggleLightTheme() function int the Application class

--- app/src/main/java/com/uxstate/recipeapp/RecipeApp.kt
+++ app/src/main/java/com/uxstate/recipeapp/RecipeApp.kt
@@ -1,15 +1,24 @@
 package com.uxstate.recipeapp
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateOf
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 class RecipeApp:Application() {
 
+    //this should be persisted in SharedPrefs or DB
+    val isDark = mutableStateOf(false)
+
     override fun onCreate() {
         super.onCreate()
 
         Timber.plant(Timber.DebugTree())
+    }
+
+    fun  toggleLightTheme(){
+
+        isDark.value = !isDark.value
     }
 }
