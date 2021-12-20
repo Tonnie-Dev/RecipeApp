2c147d8bbc26e165f8151a177222aa1df3aa4664
Antony <vontonnie@gmail.com>
2021-12-20 04:49:47 +0300

    Add Colors

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/ui/theme/Theme.kt
@@ -1,10 +1,44 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+@SuppressLint("ConflictingOnColor")
+private val LightColorPalette = lightColors(
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Black2,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
+    onSecondary = Color.Black,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = Grey1,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Black2,
+)
+
+private val  DarkColorPalette = darkColors(
+    primary = Blue700,
+    primaryVariant = Color.White,
+    onPrimary = Color.White,
+    secondary = Black1,
+    onSecondary = Color.White,
+    error = RedErrorLight,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = Black1,
+    onSurface = Color.White,
+)
+
+/*
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -17,15 +51,18 @@ private val LightColorPalette = lightColors(
     primaryVariant = Purple700,
     secondary = Teal200
 
-    /* Other default colors to override
+    */
+/* Other default colors to override
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+    *//*
+
 )
+*/
 
 @Composable
 fun RecipeAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
