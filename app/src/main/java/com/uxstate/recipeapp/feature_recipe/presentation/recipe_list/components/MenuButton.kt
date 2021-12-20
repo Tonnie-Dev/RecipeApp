370292565272954542b1d2085b03b06551f55988
Antony <vontonnie@gmail.com>
2021-12-20 19:12:51 +0300

    Add Menu Botton composable function

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MenuButton.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MenuButton.kt
@@ -1,15 +1,34 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintLayout
 
 
 @Composable
 fun MenuButton(
     modifier: Modifier = Modifier,
-    onToggleTheme: (Boolean) -> Unit
-) {
+    onToggleTheme: () -> Unit,
 
-    ConstraintLayout()
+) {
+ConstraintLayout() {
+    //create id
+    val menu = createRef()
+
+    //widget to be constrained
+    IconButton(onClick = onToggleTheme,
+        modifier.constrainAs(menu){
+            end.linkTo(parent.end)
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+        }) {
+
+        Icon(imageVector = Icons.Filled.MoreVert, contentDescription = null)
+    }
+}
+
 }
