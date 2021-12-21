31696a12136995e16d2d807f88cb9addb5aea96d
Antony <vontonnie@gmail.com>
2021-12-21 22:34:11 +0300

    Add BottomNavigation Items

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MyBottomBar.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MyBottomBar.kt
@@ -1,16 +1,37 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
 import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MyBottomBar() {
 
-    //Returns  a BottomNavigation
+    //Returns  a BottomNavigation composable
 
 
     BottomNavigation(elevation = 12.dp) {
 
+        BottomNavigationItem(
+            selected = true,
+            onClick = { /*TODO*/ },
+            icon = { Icon(imageVector = Icons.Default.Favorite, contentDescription = null )  }
+        )
+        BottomNavigationItem(
+            selected = true,
+            onClick = { /*TODO*/ },
+            icon = { Icon(imageVector = Icons.Default.Search, contentDescription = null )  }
+        )
+        BottomNavigationItem(
+            selected = true,
+            onClick = { /*TODO*/ },
+            icon = { Icon(imageVector = Icons.Default.Face, contentDescription = null)  }
+        )
     }
 }
