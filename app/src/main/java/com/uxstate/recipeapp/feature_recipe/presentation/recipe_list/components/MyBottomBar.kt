c57aacfd4e6318cca29092a26a4ca884a06a8ea9
Antony <vontonnie@gmail.com>
2021-12-21 22:27:46 +0300

    Add MyBottomBar composable

---
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MyBottomBar.kt
@@ -0,0 +1,9 @@
+package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyBottomBar() {
+
+    //Takes a BottomNavigation Item
+}
