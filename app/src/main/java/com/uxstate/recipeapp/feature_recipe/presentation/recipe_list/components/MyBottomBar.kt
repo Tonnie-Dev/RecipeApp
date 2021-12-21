87f9d353b9258f5ac8975b799de3eb00e10ef095
Antony <vontonnie@gmail.com>
2021-12-21 22:30:16 +0300

    Add BottomNavigation compossable

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MyBottomBar.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/MyBottomBar.kt
@@ -1,9 +1,16 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+import androidx.compose.material.BottomNavigation
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MyBottomBar() {
 
-    //Takes a BottomNavigation Item
+    //Returns  a BottomNavigation
+
+
+    BottomNavigation(elevation = 12.dp) {
+
+    }
 }
