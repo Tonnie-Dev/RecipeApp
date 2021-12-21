bb925595c78eb8e33bb0584b3330b360837229cb
Antony <vontonnie@gmail.com>
2021-12-21 07:41:37 +0300

    Define the font family varible

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.uxstate.recipeapp.feature_recipe.presentation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.uxstate.recipeapp.R
 
 // Set of Material typography styles to start with
 /*val Typography = Typography(
@@ -28,4 +30,8 @@ import androidx.compose.ui.unit.sp
 )*/
 
 
-val
+private val QuickSandFontFamily = FontFamily(
+    Font(R.font.quicksand_light, FontWeight.W300),
+    Font(R.font.quicksand_regular, FontWeight.W400),
+    Font(R.font.quicksand_medium, FontWeight.W500),
+    Font(R.font.quicksand_bold, FontWeight.W600))
