6a1f44737c64d2436fe48bb284cfc85269167e5e
Antony <vontonnie@gmail.com>
2021-12-22 08:21:53 +0300

    Add Default Snackbar Composable

---
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/DefaultSnackbBar.kt
@@ -0,0 +1,15 @@
+package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
+
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun DefaultSnackbar(
+    modifier: Modifier = Modifier,
+    snackbarHostState: SnackbarHostState,
+    onDismiss: () -> Unit
+) {
+
+}
