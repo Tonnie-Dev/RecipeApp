29cd2fce65b0312681ef973261bc3a6dcdcda2a9
Antony <vontonnie@gmail.com>
2021-12-24 20:58:48 +0300

    Abstract Recipe List

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/RecipeList.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe_list/components/RecipeList.kt
@@ -1,2 +1,39 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.components
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
+import com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.viewmodel.PAGE_SIZE
+import com.uxstate.recipeapp.feature_recipe.presentation.recipe_list.viewmodel.RecipeListEvent
+import timber.log.Timber
+
+@Composable
+fun RecipeList(recipes:List<Recipe>,
+onChangeRecipeScrollPosition:(Int)-> Unit,
+page:Int,
+loading:Boolean,
+               onTriggerEvent:(RecipeListEvent)-> Unit   ) {
+
+
+    LazyColumn() {
+
+        itemsIndexed(items = recipes) { position, recipe ->
+
+
+            onChangeRecipeScrollPosition(position)
+
+
+            //trigger for pagination
+            if ((position + 1) >= (page * PAGE_SIZE) && !loading) {
+
+              onTriggerEvent(RecipeListEvent.NextPageEvent)
+
+
+            }
+
+            RecipeCard(recipe = recipe) {}
+        }
+
+    }
+}
