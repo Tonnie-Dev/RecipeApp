134d0b560453485aa6b381ef98f775a8df5cb1e5
Antony <vontonnie@gmail.com>
2021-12-25 21:04:38 +0300

    Add the rating TextView

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeDetailsSection.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeDetailsSection.kt
@@ -5,40 +5,71 @@ import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
 
-class RecipeDetailsSection {
 
-    @Composable
-    fun RecipeDetailsSection(recipe: Recipe) {
+@Composable
+fun RecipeDetailsSection(recipe: Recipe) {
 
 
-        Column(modifier = Modifier
+    Column(
+        modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)) {
+                .padding(8.dp)
+    ) {
 
-           Row(
-               modifier = Modifier.fillMaxWidth(),
-               horizontalArrangement = SpaceBetween
-           ) {
-               Text(text = recipe.title, style = MaterialTheme.typography.h5)
-               Text(text = recipe.rating.toString(), style = MaterialTheme.typography.h6)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = SpaceBetween,
 
-           }
+            ) {
+            Text(
+                text = recipe.title,
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier
+                        .fillMaxWidth(0.85f)
+                        //force text to left as much as possible
+                        .wrapContentWidth(Alignment.Start)
+            )
+            Text(
+                text = recipe.rating.toString(),
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.End)
+                        .align(CenterVertically)
+            )
 
         }
 
-
-
     }
+
+
 }
 
 
 @Preview(name = "RecipeDetails Preview")
 @Composable
 fun RecipeDetailsSectionPreview() {
-    
+
+    val recipe1 = Recipe(
+        id = 13,
+        title = "Ugali Sukuma",
+        publisher = "GOAT",
+        featuredImage = "",
+        rating = 0,
+        sourceUrl = "",
+        description = "",
+        cookingInstructions = "",
+        ingredients = listOf("Carrots", "Kales", "Flour", "Kabiri-biri"),
+        dateAdded = "25.12.2021",
+        dateUpdate = "25.12.2021"
+    )
+
+    RecipeDetailsSection(recipe1)
 }
