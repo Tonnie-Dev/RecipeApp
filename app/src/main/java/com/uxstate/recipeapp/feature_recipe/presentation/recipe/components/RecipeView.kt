6444fdebc1ad69e5a2ccf425c978a89c715a8b1f
Antony <vontonnie@gmail.com>
2021-12-25 20:20:47 +0300

    Add RecipeView Composable

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeView.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeView.kt
@@ -1,2 +1,10 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe.components
 
+import androidx.compose.runtime.Composable
+import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
+import com.uxstate.recipeapp.feature_recipe.presentation.recipe.viewmodel.RecipeViewModel
+
+@Composable
+fun RecipeView(recipe: Recipe) {
+
+}
