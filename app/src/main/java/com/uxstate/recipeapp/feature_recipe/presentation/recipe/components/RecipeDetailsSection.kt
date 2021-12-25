3077f56e1f4c0d26bd09a996fc468c12a88cb412
Antony <vontonnie@gmail.com>
2021-12-25 20:40:42 +0300

    Add RecipeDetailsSection composable

---
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeDetailsSection.kt
@@ -0,0 +1,11 @@
+package com.uxstate.recipeapp.feature_recipe.presentation.recipe.components
+
+import androidx.compose.runtime.Composable
+
+class RecipeDetailsSection {
+
+    @Composable
+    fun RecipeDetailsSection() {
+
+    }
+}
--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeView.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeView.kt
@@ -1,10 +1,40 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.uxstate.recipeapp.R
 import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
-import com.uxstate.recipeapp.feature_recipe.presentation.recipe.viewmodel.RecipeViewModel
 
 @Composable
-fun RecipeView(recipe: Recipe) {
+fun RecipeView(recipe: Recipe, scrollState: ScrollState) {
+
+    Column(
+        modifier = Modifier
+                .verticalScroll(scrollState)
+                .fillMaxWidth()
+    ) {
+
+        //load image
+
+        Image(
+            painter = rememberImagePainter(
+                data = recipe.featuredImage,
+                builder = { crossfade(true)
+                placeholder(R.drawable.loading_animation)}),
+            contentDescription =null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(250.dp).fillMaxWidth()
+        )
+
+    }
 
 }
