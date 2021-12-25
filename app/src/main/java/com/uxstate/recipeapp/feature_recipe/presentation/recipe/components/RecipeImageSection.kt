5e728a0a9f3012706e71bdb98235aaf9aa68e80b
Antony <vontonnie@gmail.com>
2021-12-25 21:16:37 +0300

    Remove extra arguments form RecipeImageSection

--- app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeImageSection.kt
+++ app/src/main/java/com/uxstate/recipeapp/feature_recipe/presentation/recipe/components/RecipeImageSection.kt
@@ -1,11 +1,9 @@
 package com.uxstate.recipeapp.feature_recipe.presentation.recipe.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -15,12 +13,10 @@ import com.uxstate.recipeapp.R
 import com.uxstate.recipeapp.feature_recipe.domain.model.Recipe
 
 @Composable
-fun RecipeView(recipe: Recipe, scrollState: ScrollState) {
+fun RecipeView(recipe: Recipe) {
 
     Column(
-        modifier = Modifier
-                .verticalScroll(scrollState)
-                .fillMaxWidth()
+
     ) {
 
         //load image
@@ -28,11 +24,15 @@ fun RecipeView(recipe: Recipe, scrollState: ScrollState) {
         Image(
             painter = rememberImagePainter(
                 data = recipe.featuredImage,
-                builder = { crossfade(true)
-                placeholder(R.drawable.loading_animation)}),
-            contentDescription =null,
+                builder = {
+                    crossfade(true)
+                    placeholder(R.drawable.loading_animation)
+                }),
+            contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier.height(250.dp).fillMaxWidth()
+            modifier = Modifier
+                    .height(250.dp)
+                    .fillMaxWidth()
         )
 
     }
